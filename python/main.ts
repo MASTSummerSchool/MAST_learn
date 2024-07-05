227a7175a7932d00b1eff9d6f1c6412e08b32e5c
lozingaro <28324421+lozingaro@users.noreply.github.com>
2024-07-05 16:17:17 +0200

    filename and labels fixed

--- python/main.ts
+++ python/main.ts
@@ -1,19 +1,19 @@
 //% color="#F7AD45" iconWidth=50 iconHeight=40
 namespace robot {
 
-    //% block="Train decision tree from [FILENAME] where target name is [TARGET]" blockType="reporter"
+    //% block="Train decision tree from [FILENAME] where target name is [LABEL]" blockType="reporter"
     //% FILENAME.shadow="string" FILENAME.defl="data"
-    //% TARGET.shadow="string" TARGET.defl="target"
+    //% LABEL.shadow="string" LABEL.defl="label"
     export function train_decision_tree(parameter: any, block: any) {
         // Initiating the variables as strings removing trailing and leading spaces
         let filename = parameter.FILENAME.code;
-        let target = parameter.TARGET.code;
+        let label = parameter.LABEL.code;
 
         // Add import statement
         Generator.addImport(`from learn import train_decision_tree`);
 
         // Add the actual code for the training
-        Generator.addCode(`train_decision_tree(${filename}, ${target})`);
+        Generator.addCode(`train_decision_tree(${filename}, ${label})`);
 
         // Add comments OS dependant
         Generator.addCode(`# This block LOAD the sensor data FROM:`)
@@ -26,14 +26,14 @@ namespace robot {
         return null;
     }
 
-    //% block="Train neural network with [PATH] where target name is [TARGET]" blockType="reporter"
-    //% PATH.shadow="string" PATH.defl="data"
-    //% TARGET.shadow="string" TARGET.defl="target"
+    //% block="Train neural network with [FILENAME] where target name is [LABEL]" blockType="reporter"
+    //% FILENAME.shadow="string" FILENAME.defl="data"
+    //% LABEL.shadow="string" LABEL.defl="label"
     export function train_neural_network(parameter: any, block: any) {
-        let data_path = parameter.PATH.code;
-        let target = parameter.TARGET.code;
+        let filename = parameter.FILENAME.code;
+        let label = parameter.LABEL.code;
         Generator.addImport(`from learn import train_neural_network`);
-        Generator.addCode(`train_neural_network(${data_path}, ${target})`);
+        Generator.addCode(`train_neural_network(${filename}, ${label})`);
     }
 
     //% block="Infer label with [MODEL] from condition [CONDITION]" blockType="command"
